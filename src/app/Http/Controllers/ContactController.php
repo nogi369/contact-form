d6c40a4bf4c9986ad048d07db541e30a8e784dff
nogi <Createc369.Re@gmail.com>
2024-07-02 23:39:38 +0900

    フォーム入力ページの送信ボタンを押した時の処理を作成

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -10,4 +10,12 @@ class ContactController extends Controller
   {
     return view('index');
   }
+
+public function confirm(Request $request)
+  {
+    $contact = $request->only(['name', 'email', 'tel', 'content']);
+    // return $contact;
+    // return view('confirm', ['contact' => $contact]);
+    return view('confirm', compact('contact'));
+  }
 }
