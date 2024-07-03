967ba30a391c314b2027f4e6f989d00ce9ff700d
nogi <Createc369.Re@gmail.com>
2024-07-03 18:27:18 +0900

    レイアウトの共通化

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,25 +1,14 @@
 <!DOCTYPE html>
 <html lang="ja">
+  @extends('layouts.app')
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Contact Form</title>
-  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  @section('css')
   <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
-</head>
+  @endsection
 
 <body>
-  <header class="header">
-    <div class="header__inner">
-      <a class="header__logo" href="/">
-        Contact Form
-      </a>
-    </div>
-  </header>
-
   <main>
+    @section('content')
     <div class="contact-form__content">
       <div class="contact-form__heading">
         <h2>お問い合わせ</h2>
@@ -80,7 +69,7 @@
           </div>
           <div class="form__group-content">
             <div class="form__input--textarea">
-              <textarea name="content" placeholder="資料をいただきたいです"></textarea>
+              <textarea name="content" placeholder="資料をいただきたいです">{{ old('content') }}</textarea>
             </div>
           </div>
         </div>
@@ -89,6 +78,7 @@
         </div>
       </form>
     </div>
+    @endsection
   </main>
 </body>
 
